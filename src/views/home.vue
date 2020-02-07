e5b63219086d38c11cd10bbdb80c3c8213ce61bf
Witchapon Wongsriskulchai <witchapon.wongsriskulchai@refinitiv.com>
2020-02-07 17:05:46 +0700

    Inital commit

--- src/views/home.vue
+++ src/views/home.vue
@@ -12,29 +12,44 @@ import axios from "axios";
 export default {
   data() {
     return {
-      sum_percent_qUse: 0
+      sum_percent_qUse: 0,
+      date: ""
     };
   },
   methods: {
-    getPercentUse() {
+    getWaterPercentUse(date) {
+      let url = `http://localhost:3000`;
       axios
-        .get("http://localhost:3000/getWater?date=2019-08-27")
+        .get(`${url}/getWater?date=${date}`)
         .then(res => {
+          console.log(res.data);
           if (res.length === 0) {
             this.sum_percent_qUse = 0;
             console.log(this.sum_percent_qUse);
           } else {
-            this.sum_percent_qUse = res.sum_all.sum_PERCENT_QUse;
+            this.sum_percent_qUse = res.data.sum_all.sum_PERCENT_QUse;
           }
         })
         .catch(err => {
           console.log(err);
           this.sum_percent_qUse = 0;
         });
+    },
+    formatDate(date) {
+      var d = new Date(date),
+        month = "" + (d.getMonth() + 1),
+        day = "" + d.getDate(),
+        year = d.getFullYear();
+
+      if (month.length < 2) month = "0" + month;
+      if (day.length < 2) day = "0" + day;
+
+      return [year, month, day].join("-");
     }
   },
   mounted() {
-    this.getPercentUse();
+    this.date = this.formatDate(new Date().toLocaleDateString());
+    this.getWaterPercentUse(this.date);
   }
 };
 </script>
